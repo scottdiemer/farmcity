ab059a537eab066dbcc6013d38034b563840ae6d
scottdiemer <sdiemer@cfl.rr.com>
2021-11-09 16:25:16 -0500

    moved hero section from home page to hero component. started building footer component

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,50 @@
+import Image from "next/image";
+import Link from "next/link";
+import links from "../constants/links";
+
 export default function Footer() {
   return (
     <>
-      <footer className="flex justify-between">This is the footer</footer>
+      <footer className="flex justify-between p-4 bg-gray-400">
+        <div className="flex items-center">
+          <Image
+            className="items-center"
+            src="/FarmCity_TextOnly.svg"
+            alt="Farm City"
+            width={200}
+            height={55}
+          />
+        </div>
+        <div>
+          <ul className="uppercase">
+            {links.map((item, index) => {
+              return (
+                <li key={index}>
+                  <Link className="cursorpointer" href={item.path}>
+                    {item.text}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <div flex flex-col>
+          <h2>Orlando Location</h2>
+          <h3>Address</h3>
+          <p>1610 Atlanta Ave</p>
+          <p>Orlando, FL 32806</p>
+          <h3>Phone</h3>
+          <p>(407) 843-7470</p>
+        </div>
+        <div flex flex-col>
+          <h2>Apopka Location</h2>
+          <h3>Address</h3>
+          <p>440 S. Park Ave</p>
+          <p>Apopka, FL 32704</p>
+          <h3>Phone</h3>
+          <p>(407) 889-2822</p>
+        </div>
+      </footer>
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,10 @@
-import Image from "next/image";
-import heroImage from "../public/img/bg-image.jpg";
 import Layout from "../components/layout";
+import Hero from "../components/hero";
 
 export default function Home() {
   return (
     <Layout title="Farm City Feed Stores" favIcon="/favIcon.ico">
-      <section
-        className="relative block"
-        style={{ height: "calc(100vh - 137px)", zIndex: "-1" }}
-      >
-        <Image src={heroImage} layout="fill" objectFit="cover" />
-        <div className="absolute flex flex-col">
-          <h1>this is our h1 tag</h1>
-          <p>
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-            et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
-            Lorem ipsum dolor sit amet.
-          </p>
-        </div>
-      </section>
+      <Hero />
     </Layout>
   );
 }
