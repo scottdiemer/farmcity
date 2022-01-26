bdbf791973e7c09045e9f250d4d9de84c2aaccbe
scottdiemer <sdiemer@cfl.rr.com>
2022-01-25 21:28:38 -0500

    Installed and setup react-query

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
+import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
+import { useState } from "react";
 import "tailwindcss/tailwind.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const [queryClient] = useState(() => new QueryClient());
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+    </QueryClientProvider>
+  );
 }
 
 export default MyApp;
