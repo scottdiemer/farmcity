a2ef2cffb7c4b7085004ad8460b1a0093ff3dc6a
scottdiemer <sdiemer@cfl.rr.com>
2022-01-27 16:00:42 -0500

    Removed react-query and graphql packages until I get a working understanding.

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,7 @@
-import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
-import { useState } from "react";
 import "tailwindcss/tailwind.css";
 
 function MyApp({ Component, pageProps }) {
-  const [queryClient] = useState(() => new QueryClient());
-  return (
-    <QueryClientProvider client={queryClient}>
-      <Hydrate state={pageProps.dehydratedState}>
-        <Component {...pageProps} />
-      </Hydrate>
-    </QueryClientProvider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
