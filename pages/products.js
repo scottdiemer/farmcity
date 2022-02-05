9cef45bd2efaa33d929373a45b64f5e146ffe57c
scottdiemer <sdiemer@cfl.rr.com>
2022-02-05 14:43:33 -0500

    Wrote json fetching method to replace using graphql-request

--- pages/products.js
+++ pages/products.js
@@ -1,17 +1,13 @@
 import { dehydrate, QueryClient, useQuery } from "react-query";
-import { getProducts } from "../lib/products";
+import { getProducts } from "../lib/products2";
 import Title from "../components/Title";
 import SlideInWhenVisible from "../components/SlideInWhenVisible";
 import Layout from "../components/Layout";
 import Card from "../components/Card";
 
 export default function Products({ serverUrl }) {
-  const {
-    data: { products },
-    error,
-  } = useQuery("products", getProducts);
-
-  !products && <p>No data!</p>;
+  const { data, error } = useQuery("products", getProducts);
+  !data && <p>No data!</p>;
   error && <p>Oops something went wrong!</p>;
 
   return (
@@ -19,7 +15,7 @@ export default function Products({ serverUrl }) {
       <section className="bg-tan/25 overflow-hidden">
         <Title className="text-shadow" title="Products" />
         <div className="container p-6 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {products.map((product, index) => {
+          {data.data.products.map((product, index) => {
             return (
               <SlideInWhenVisible number={index} key={index}>
                 <Card
