30bff73c672c933c4689c928799b5a9cd38d0dda
scottdiemer <sdiemer@cfl.rr.com>
2022-03-22 09:14:28 -0400

    Added API Key to fetch request

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ module.exports = {
     GRAPHQL_ENDPOINT: process.env.GRAPHQL_ENDPOINT,
     SERVER_URL: process.env.SERVER_URL,
     REVALIDATE_SECONDS: process.env.REVALIDATE_SECONDS,
+    SESSION_SECRET: process.env.SESSION_SECRET,
   },
   images: {
     domains: ["i.imgur.com", "localhost"],
