32c2982c0533f9b8bba3fc90bf229cfa92b0c70f
scottdiemer <sdiemer@cfl.rr.com>
2022-03-22 15:17:31 -0400

    Added preconstruct and did rewrite in next config for path to server

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
-module.exports = {
+const withPreconstruct = require("@preconstruct/next");
+
+const serverUrl = process.env.SERVER_URL || "http://localhost:5000";
+
+const nextConfig = {
   env: {
     GRAPHQL_ENDPOINT: process.env.GRAPHQL_ENDPOINT,
     SERVER_URL: process.env.SERVER_URL,
@@ -12,4 +16,10 @@ module.exports = {
     locales: ["en"],
     defaultLocale: "en",
   },
+  rewrites: () => [
+    { source: "/api/graphql", destination: `${serverUrl}/api/graphql` },
+  ],
+  ignoreBuildErrors: true,
 };
+
+module.exports = withPreconstruct({ ...nextConfig });
